30682521f71b76f2f27ad31c5b1f890e0babfcc2
daniel30-07 <danielborgesdecarvalho@gmail.com>
2020-08-21 11:31:17 -0300

    Style: comments

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'))
 seedDB();
 
 
